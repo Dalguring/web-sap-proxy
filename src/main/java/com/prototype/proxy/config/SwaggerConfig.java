052b960329006947c2e3bb1bd92be19377905949
dalgee <blussm@kakao.com>
2025-12-27 19:40:59 +0900

    feat: swagger-ui, api-docs 추가 - SwaggerConfig 파일 추가 refactor: FieldMapping 클래스 내재화 - 따로 분리 돼있던 FieldMapping 클래스를 InterfaceDefinition 내부로 옮겨 사용

---
+++ src/main/java/com/prototype/proxy/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.prototype.proxy.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("http://localhost:8080");
+        devServer.setDescription("dev");
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Interface Proxy API")
+                        .version("v1")
+                        .description("SAP RFC 호출을 위한 인터페이스 프록시 서버 API 문서"))
+                .servers(List.of(devServer));
+    }
+}
